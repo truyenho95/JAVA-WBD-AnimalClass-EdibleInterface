2f5d1f634b6f87c7c203bdeabf38def9a5be8dbd
truyenho95 <truyenho.webdev@gmail.com>
2018-12-10 11:14:56 +0700

    Add animal package

---
+++ src/animal/model/Animal.java
@@ -0,0 +1,5 @@
+package animal.model;
+
+public abstract class Animal {
+  public abstract String makeSound();
+}
---
+++ src/animal/model/Tiger.java
@@ -0,0 +1,8 @@
+package animal.model;
+
+public class Tiger extends Animal {
+  @Override
+  public String makeSound() {
+    return "Tiger: roarrrr!";
+  }
+}
